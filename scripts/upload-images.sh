bf92ba8a8aa8ce91762603266bb8d8a7066b448e
liuyu <>
2025-02-12 20:28:38 +0800

    ci: debug upload image

--- scripts/upload-images.sh
+++ scripts/upload-images.sh
@@ -27,6 +27,7 @@ cat $1|while read image; do
                 exit 1
             fi
 
+            echo "start to upload [$name.tar.gz]"
             aws s3 cp $name.tar.gz s3://terminus-os-install/$path$name.tar.gz --acl=public-read
             aws s3 cp $name.tar.gz s3://terminus-os-install/backup/$path$backup_file --acl=public-read
             aws s3 cp $checksum s3://terminus-os-install/$path$checksum --acl=public-read
@@ -48,7 +49,7 @@ cat $1|while read image; do
     if [ $? -ne 0 ]; then
         code=$(curl -o /dev/null -fsSLI -w "%{http_code}" https://dc3p1870nn3cj.cloudfront.net/$path$checksum)
         if [ $code -eq 403 ]; then
-            set -e
+            set -ex
             docker pull $image
             docker save $image -o $name.tar
             gzip $name.tar
@@ -64,7 +65,7 @@ cat $1|while read image; do
             aws s3 cp $name.tar.gz s3://terminus-os-install/backup/$path$backup_file --acl=public-read
             aws s3 cp $checksum s3://terminus-os-install/$path$checksum --acl=public-read
             echo "upload $name completed"
-            set +e
+            set +ex
         else
             if [ $code -ne 200  ]; then
                 echo  "failed to check image"
