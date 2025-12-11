18310c4083b93afe16ee63a56e04b21c77ac18c7
Meow33 <supermonkey03@163.com>
2025-12-11 22:03:37 +0800

    docs: add larepass upgrade restart prompt

--- docs/.vitepress/en.ts
+++ docs/.vitepress/en.ts
@@ -156,10 +156,10 @@ const side = {
             {text: "Generate 2FA codes", link: "/manual/larepass/two-factor-verification"},
           ],
         },
-        {
+        /*{
           text: "Manage knowledge",
           link: "/manual/larepass/manage-knowledge",
-        },
+        },*/
       ],
     },
     {
