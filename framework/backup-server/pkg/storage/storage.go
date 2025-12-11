93b3ff3cc050152f7a6a7aed6b747f77e307920c
dkeven <82354774+dkeven@users.noreply.github.com>
2025-12-11 21:13:30 +0800

    refactor: integrate module backup server into main repo (#2211)

---
+++ framework/backup-server/pkg/storage/storage.go
@@ -0,0 +1,11 @@
+package storage
+
+const (
+	progressDone     = 10000 //
+	progressInterval = 5
+)
+
+var (
+	FreeLimit     float64 = 85.00
+	ConnectErrors         = []string{"dial tcp", "connect:"}
+)
