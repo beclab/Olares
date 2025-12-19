4661b32bf4dc1493f1fe892eb033e4c3f5c0bd8b
hysyeah <hysyeah@gmail.com>
2025-12-19 23:37:07 +0800

    app-service: fix pending canceled namespace delete (#2283)
    fix: pending canceled namespace delete

--- framework/app-service/pkg/appstate/pending_cancel_failed_app.go
+++ framework/app-service/pkg/appstate/pending_cancel_failed_app.go
@@ -1,18 +1,25 @@
 package appstate
 
 import (
+	"context"
 	"time"
 
 	appsv1 "bytetrade.io/web3os/app-service/api/app.bytetrade.io/v1alpha1"
+	apputils "bytetrade.io/web3os/app-service/pkg/utils/app"
+
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // FIXME: impossible state
 
-var _ StatefulApp = &PendingCancelFailedApp{}
+var _ OperationApp = &PendingCancelFailedApp{}
 
 type PendingCancelFailedApp struct {
-	*DoNothingApp
+	*baseOperationApp
 }
 
 func NewPendingCancelFailedApp(c client.Client,
@@ -20,7 +27,8 @@ func NewPendingCancelFailedApp(c client.Client,
 	return appFactory.New(c, manager, 0,
 		func(c client.Client, manager *appsv1.ApplicationManager, ttl time.Duration) StatefulApp {
 			return &PendingCancelFailedApp{
-				DoNothingApp: &DoNothingApp{
+				baseOperationApp: &baseOperationApp{
+					ttl: ttl,
 					baseStatefulApp: &baseStatefulApp{
 						manager: manager,
 						client:  c,
@@ -30,11 +38,25 @@ func NewPendingCancelFailedApp(c client.Client,
 		})
 }
 
-//func (p *PendingCancelFailedApp) Exec(ctx context.Context) (StatefulInProgressApp, error) {
-//	// FIXME: should set a max retry count for cancel operation
-//	err := p.updateStatus(ctx, p.manager, appsv1.PendingCanceling, nil, appsv1.PendingCanceling.String())
-//	if err != nil {
-//		klog.Errorf("update app manager %s to %s state failed %v", p.manager.Name, appsv1.PendingCanceling, err)
-//	}
-//	return nil, err
-//}
+func (p *PendingCancelFailedApp) Exec(ctx context.Context) (StatefulInProgressApp, error) {
+	if !apputils.IsProtectedNamespace(p.manager.Spec.AppNamespace) {
+		var ns corev1.Namespace
+		err := p.client.Get(ctx, types.NamespacedName{Name: p.manager.Spec.AppNamespace}, &ns)
+		if err != nil && !apierrors.IsNotFound(err) {
+			return nil, err
+		}
+		if err == nil {
+			e := p.client.Delete(ctx, &ns)
+			if e != nil {
+				klog.Errorf("failed to delete ns %s, err=%v", p.manager.Spec.AppNamespace, e)
+				return nil, e
+			}
+		}
+	}
+
+	return nil, nil
+}
+
+func (p *PendingCancelFailedApp) Cancel(ctx context.Context) error {
+	return nil
+}
