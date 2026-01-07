c31d495c22776e6459e38c305f4c70821429bbe1
Power-One-2025 <zhengchunhong@bytetrade.io>
2026-01-07 20:27:28 +0800

    docs/update/fix-build-conflict

--- docs/.vitepress/en.ts
+++ docs/.vitepress/en.ts
@@ -462,80 +462,6 @@ const side = {
     },
   ],
   "/use-cases/": [
-  {
-    text: "Use cases",
-    link: "/use-cases/",
-    items: [
-      {
-        text: "Stable Diffusion",
-        link: "/use-cases/stable-diffusion",
-      },
-      {
-        text: "ComfyUI",
-        link: "/use-cases/comfyui",
-        collapsed: true,
-        items: [
-          {
-            text: "Manage ComfyUI",
-            link: "/use-cases/comfyui-launcher",
-          },
-          {
-            text: "Use ComfyUI for Krita",
-            link: "/use-cases/comfyui-for-krita",
-          },
-        ]
-      },
-      {
-        text: "Ollama",
-        link: "/use-cases/ollama",
-      },
-      {
-        text: "Open WebUI",
-        link: "/use-cases/openwebui",
-      },
-      {
-        text: "Perplexica",
-        link: "/use-cases/perplexica",
-      },
-      {
-        text: "Dify",
-        link: "/use-cases/dify",
-      },
-      {
-        text: "Jellyfin",
-        link: "/use-cases/stream-media",
-      },
-      {
-        text: "Steam",
-        link: "/use-cases/stream-game",
-      },
-      // {
-      //   text: "Redroid",
-      //   link: "/use-cases/host-cloud-android",
-      // },
-      {
-        text: "Windows",
-        link: "/use-cases/windows",
-      }, 
-      {
-        text: "DeerFlow",
-        link: "/use-cases/deerflow",
-      }, 
-      {
-        text: "Duix.Avatar",
-        link: "/use-cases/duix-avatar",
-      },
-      {
-        text: "ACE-Step",
-        link: "/use-cases/ace-step",
-      }, 
-      {
-        text: "PDFMathTranslate",
-        link: "/use-cases/pdfmathtranslate",
-      },     
-    ],
-  },
- ],
     {
       text: "Use cases",
       link: "/use-cases/",
@@ -612,6 +538,10 @@ const side = {
         {
           text: "ACE-Step",
           link: "/use-cases/ace-step",
+        },
+        {
+          text: "PDFMathTranslate",
+          link: "/use-cases/pdfmathtranslate",
         },
       ],
     },
