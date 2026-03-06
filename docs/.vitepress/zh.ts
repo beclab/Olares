1c347ea0e29175700101777f8066d70840fa1949
Power-One-2025 <zhengchunhong@bytetrade.io>
2026-03-06 22:10:01 +0800

    docs: add ZH version for troubleshooting topic (#2641)
    * initial draft
    
    * refine in progress
    
    * add zh version for troubleshooting
    
    * Apply suggestions from code review
    
    Co-authored-by: Meow33 <supermonkey03@163.com>
    
    * address comments
    
    * address comments
    
    * fix link
    
    ---------
    
    Co-authored-by: Meow33 <supermonkey03@163.com>

--- docs/.vitepress/zh.ts
+++ docs/.vitepress/zh.ts
@@ -51,7 +51,11 @@ const side = {
             {
               text: "LarePass VPN 无法使用",
               link: "/zh/manual/help/ts-larepass-vpn-not-working",
-            }
+            },
+            {
+              text: "网络尚未准备好或 olares 连接错误",
+              link: "/zh/manual/help/ts-network-not-ready",
+            }            
           ],
         },
       ],
