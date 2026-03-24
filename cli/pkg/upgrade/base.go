d34ef65041cd2143039535c583cecd39ac344f88
hys <hysyeah@gmail.com>
2026-03-24 20:26:39 +0800

    set upgrade l4 retry

--- cli/pkg/upgrade/base.go
+++ cli/pkg/upgrade/base.go
@@ -143,7 +143,7 @@ func (u upgraderBase) UpgradeSystemComponents() []task.Interface {
 		&task.LocalTask{
 			Name:   "UpgradeL4BFLProxy",
 			Action: &upgradeL4BFLProxy{Tag: "v0.3.13"},
-			Retry:  5,
+			Retry:  6,
 			Delay:  15 * time.Second,
 		},
 	}
