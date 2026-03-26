3ba8aa689f3c77773974948908dfb61de82c2424
dkeven <dkvvven@gmail.com>
2026-03-26 19:37:23 +0800

    fix(cli): skip systemd daemon reload during upgrade

--- cli/pkg/upgrade/1_12_5.go
+++ cli/pkg/upgrade/1_12_5.go
@@ -8,7 +8,6 @@ import (
 	"github.com/beclab/Olares/cli/pkg/core/task"
 	"github.com/beclab/Olares/cli/pkg/etcd/templates"
 	"github.com/beclab/Olares/cli/pkg/gpu"
-	"github.com/beclab/Olares/cli/pkg/terminus"
 	"github.com/beclab/Olares/cli/version"
 )
 
@@ -46,13 +45,6 @@ func (u upgrader_1_12_5) PrepareForUpgrade() []task.Interface {
 				Name:     "GenerateETCDService",
 				Template: templates.ETCDService,
 				Dst:      "/etc/systemd/system/etcd.service",
-			},
-		},
-		&task.LocalTask{
-			Name: "ReloadSystemd",
-			Desc: "Reload systemd",
-			Action: &terminus.SystemctlCommand{
-				DaemonReloadPreExec: true,
 			},
 		},
 	}, u.upgraderBase.PrepareForUpgrade()...)
