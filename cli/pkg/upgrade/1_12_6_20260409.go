2e174262d86da46427537f3a2fa803b4fdeeda05
hysyeah <hysyeah@gmail.com>
2026-04-09 23:41:38 +0800

    feat(cli): add cpu rapl constraint metric (#2845)

--- cli/pkg/upgrade/1_12_6_20260409.go
+++ cli/pkg/upgrade/1_12_6_20260409.go
@@ -5,15 +5,15 @@ import (
 	"github.com/beclab/Olares/cli/pkg/core/task"
 )
 
-type upgrader_1_12_6_20260403 struct {
+type upgrader_1_12_6_20260409 struct {
 	breakingUpgraderBase
 }
 
-func (u upgrader_1_12_6_20260403) Version() *semver.Version {
-	return semver.MustParse("1.12.6-20260403")
+func (u upgrader_1_12_6_20260409) Version() *semver.Version {
+	return semver.MustParse("1.12.6-20260409")
 }
 
-func (u upgrader_1_12_6_20260403) PrepareForUpgrade() []task.Interface {
+func (u upgrader_1_12_6_20260409) PrepareForUpgrade() []task.Interface {
 	tasks := make([]task.Interface, 0)
 	tasks = append(tasks, upgradeKsConfig()...)
 	tasks = append(tasks, upgradeKubernetesPrometheusRule()...)
@@ -26,5 +26,5 @@ func (u upgrader_1_12_6_20260403) PrepareForUpgrade() []task.Interface {
 }
 
 func init() {
-	registerDailyUpgrader(upgrader_1_12_6_20260403{})
+	registerDailyUpgrader(upgrader_1_12_6_20260409{})
 }
