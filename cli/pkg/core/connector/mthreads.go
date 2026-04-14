f874707bef44a756323329dafb64911a2bb8ff00
eball <liuy102@hotmail.com>
2026-04-14 17:29:09 +0800

    feat(gpu): add MThreads GPU support and related modules

--- cli/pkg/core/connector/mthreads.go
+++ cli/pkg/core/connector/mthreads.go
@@ -1,20 +1,23 @@
 package connector
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
-func isMthreadsAIBook(execRuntime Runtime) bool {
-	dmidecode, err := execRuntime.GetRunner().SudoCmd("command -v dmidecode", false, false)
+func isMthreadsAIBook(cmdExec func(s string) (string, error)) bool {
+	dmidecode, err := cmdExec("command -v dmidecode")
 	if err != nil {
 		fmt.Printf("Error executing dmidecode command: %v\n", err)
 		return false
 	}
 
-	if dmidecode == "" {
+	if len(dmidecode) == 0 {
 		fmt.Println("dmidecode command not found, cannot determine if it's an Mthreads AI Book.")
 		return false
 	}
 
-	output, err := execRuntime.GetRunner().SudoCmd("dmidecode -s processor-manufacturer", false, false)
+	output, err := cmdExec("dmidecode -s processor-manufacturer")
 	if err != nil {
 		fmt.Printf("Error executing dmidecode to get processor manufacturer: %v\n", err)
 		return false
@@ -23,16 +26,32 @@ func isMthreadsAIBook(execRuntime Runtime) bool {
 	return output == "AIBOOK"
 }
 
-func isMthreadsAIBookM1000(execRuntime Runtime) bool {
-	if !isMthreadsAIBook(execRuntime) {
+func isMthreadsAIBookM1000(cmdExec func(s string) (string, error)) bool {
+	if !isMthreadsAIBook(cmdExec) {
 		return false
 	}
 
-	output, err := execRuntime.GetRunner().SudoCmd("dmidecode -s processor-version", false, false)
+	output, err := cmdExec("dmidecode -s processor-version")
 	if err != nil {
 		fmt.Printf("Error executing dmidecode to get processor version: %v\n", err)
 		return false
 	}
 
 	return output == "M1000"
+}
+
+func IsMThreadsAIBookM1000Local() bool {
+	return isMthreadsAIBookM1000(func(s string) (string, error) {
+		out, err := exec.Command("sh", "-c", s).Output()
+		if err != nil {
+			return "", err
+		}
+		return string(out), nil
+	})
+}
+
+func IsMThreadsAIBookM1000(execRuntime Runtime) bool {
+	return isMthreadsAIBookM1000(func(s string) (string, error) {
+		return execRuntime.GetRunner().SudoCmd(s, false, false)
+	})
 }
--- cli/pkg/gpu/types.go
+++ cli/pkg/gpu/types.go
@@ -12,9 +12,10 @@ var (
 )
 
 const (
-	NvidiaCardType    = "nvidia"      // handling by HAMi
-	AmdGpuCardType    = "amd-gpu"     //
-	AmdApuCardType    = "amd-apu"     // AMD APU with integrated GPU , AI Max 395 etc.
-	GB10ChipType      = "nvidia-gb10" // NVIDIA GB10 Superchip & unified system memory
-	StrixHaloChipType = "strix-halo"  // AMD Strix Halo GPU & unified system memory
+	NvidiaCardType    = "nvidia"         // handling by HAMi
+	AmdGpuCardType    = "amd-gpu"        //
+	AmdApuCardType    = "amd-apu"        // AMD APU with integrated GPU , AI Max 395 etc.
+	GB10ChipType      = "nvidia-gb10"    // NVIDIA GB10 Superchip & unified system memory
+	StrixHaloChipType = "strix-halo"     // AMD Strix Halo GPU & unified system memory
+	MThreadsM1000Type = "mthreads-m1000" // MThreads M1000 AI Book with integrated GPU
 )
