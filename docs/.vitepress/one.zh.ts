52f2992719d6a474a2ab92d5995277ba803932e1
Power-One-2025 <zhengchunhong@bytetrade.io>
2026-04-29 16:54:32 +0800

    docs: add BIOS blind reset guide for troubleshooting Olares One HG black screen issue (#2961)
    * add ts: cannot access BIOS due to black screen
    
    * edits for consistency

--- docs/.vitepress/one.zh.ts
+++ docs/.vitepress/one.zh.ts
@@ -18,6 +18,16 @@ export const oneSidebar: DefaultTheme.Sidebar = {
           text: "Known issues",
           link: "/zh/one/known-issues",
         },
+        {
+          text: "Troubleshooting",
+          collapsed: true,
+          items: [
+            {
+              text: "Cannot enter BIOS",
+              link: "/zh/one/ts-bios-black-screen",
+            },
+          ],
+        },         
       ],
     },
     {
