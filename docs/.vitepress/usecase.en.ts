f3fb02f823e749d97791af65d450a0bbf0667fed
Yajing <110797546+fnalways@users.noreply.github.com>
2026-05-11 19:38:55 +0800

    docs: add whisper-webui (#2786)
    * add whisper-webui
    
    * update screenshots and instructions
    
    * fix format
    
    * update tutorial.
    
    * fix links
    
    ---------
    
    Co-authored-by: Meow33 <supermonkey03@163.com>

--- docs/.vitepress/usecase.en.ts
+++ docs/.vitepress/usecase.en.ts
@@ -162,6 +162,10 @@ export const useCaseSidebar: DefaultTheme.Sidebar = {
               link: "/use-cases/pdfmathtranslate",
             },
             {
+              text: "Whisper-WebUI",
+              link: "/use-cases/whisper-webui",
+            },
+            {
               text: "Speaches",
               link: "/use-cases/speaches",
             },
