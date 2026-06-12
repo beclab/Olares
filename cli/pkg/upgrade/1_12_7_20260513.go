37915d0b7d576085d61c97435a97bcda08cd48f9
eball <liuy102@hotmail.com>
2026-06-12 14:50:38 +0800

    cherry-pick: chore(cli): update daily upgrader for cuda 13.2 (#3069)

--- cli/pkg/upgrade/1_12_7_20260513.go
+++ cli/pkg/upgrade/1_12_7_20260513.go
@@ -5,23 +5,23 @@ import (
 	"github.com/beclab/Olares/cli/pkg/core/task"
 )
 
-type upgrader_1_12_6_20260512 struct {
+type upgrader_1_12_7_20260513 struct {
 	breakingUpgraderBase
 }
 
-func (u upgrader_1_12_6_20260512) Version() *semver.Version {
-	return semver.MustParse("1.12.6-20260512")
+func (u upgrader_1_12_7_20260513) Version() *semver.Version {
+	return semver.MustParse("1.12.7-20260513")
 }
 
-func (u upgrader_1_12_6_20260512) NeedRestart() bool {
+func (u upgrader_1_12_7_20260513) NeedRestart() bool {
 	return true
 }
 
-func (u upgrader_1_12_6_20260512) PrepareForUpgrade() []task.Interface {
+func (u upgrader_1_12_7_20260513) PrepareForUpgrade() []task.Interface {
 	return u.upgraderBase.PrepareForUpgrade()
 }
 
-func (u upgrader_1_12_6_20260512) UpdateOlaresVersion() []task.Interface {
+func (u upgrader_1_12_7_20260513) UpdateOlaresVersion() []task.Interface {
 	var tasks []task.Interface
 	tasks = append(tasks,
 		&task.LocalTask{
@@ -40,5 +40,5 @@ func (u upgrader_1_12_6_20260512) UpdateOlaresVersion() []task.Interface {
 }
 
 func init() {
-	registerDailyUpgrader(upgrader_1_12_6_20260512{})
+	registerDailyUpgrader(upgrader_1_12_7_20260513{})
 }
