e0b2ac562ee72dbc12bcf8beb25d5603787a0e9c
hys <hysyeah@gmail.com>
2026-06-16 18:11:04 +0800

    update l4-bfl-proxy image tag to v0.3.30

--- cli/pkg/upgrade/base.go
+++ cli/pkg/upgrade/base.go
@@ -147,7 +147,7 @@ func (u upgraderBase) UpgradeSystemComponents() []task.Interface {
 		},
 		&task.LocalTask{
 			Name:   "UpgradeL4BFLProxy",
-			Action: &upgradeL4BFLProxy{Tag: "v0.3.29"},
+			Action: &upgradeL4BFLProxy{Tag: "v0.3.30"},
 			Retry:  6,
 			Delay:  15 * time.Second,
 		},
