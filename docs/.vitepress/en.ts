eeddde28938f8b1ea7952da24192835e17895b4e
Meow33 <supermonkey03@163.com>
2026-06-22 19:21:29 +0800

    update based on comments

--- docs/.vitepress/en.ts
+++ docs/.vitepress/en.ts
@@ -408,7 +408,7 @@ const side = {
                   link: "/manual/olares/settings/set-up-hosts",
                 },
                 {
-                  text: "Enable Overlay Gateway",
+                  text: "Manage Overlay Gateway",
                   link: "/manual/olares/settings/overlay-gateway",
                 },
               ],
