7bb825a59556294c823c5e6bc360e852b0474072
hys <hysyeah@gmail.com>
2026-06-29 18:47:06 +0800

    update l4 image tag to v0.3.34

--- cli/pkg/upgrade/base.go
+++ cli/pkg/upgrade/base.go
@@ -147,7 +147,7 @@ func (u upgraderBase) UpgradeSystemComponents() []task.Interface {
 		},
 		&task.LocalTask{
 			Name:   "UpgradeL4BFLProxy",
-			Action: &upgradeL4BFLProxy{Tag: "v0.3.33"},
+			Action: &upgradeL4BFLProxy{Tag: "v0.3.34"},
 			Retry:  6,
 			Delay:  15 * time.Second,
 		},
