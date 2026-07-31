7efa601d099b234a14e3ce295b36c05414edaa8b
eball <liuy102@hotmail.com>
2026-07-31 16:35:54 +0800

    fix(build): ensure directory creation for replaced vendor files (#3816)

--- build/build.sh
+++ build/build.sh
@@ -16,6 +16,11 @@ if [[ "${REPO_PATH}" != "" && "$REPO_PATH" != "/" ]]; then
         file=$(awk -F "$path" '{print $1$2}' <<< "$l")  
         if [[ "$file" != ".gitkeep" ]]; then
             echo "replace [$file] with [$l]"
+            replace_path=$(dirname $file)
+            if [ ! -d $replace_path ]; then
+                echo "create replace dir [$replace_path]"
+                mkdir -p $replace_path
+            fi
             cp -f "$l" "$file"
         fi
     done
