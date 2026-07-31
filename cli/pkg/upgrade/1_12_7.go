dfda4322d87b83d188c61dc0764c84f695bc233b
dkeven <82354774+dkeven@users.noreply.github.com>
2026-07-31 14:03:24 +0800

    chore(cli): add upgrader for mainline version 1.12.7 (#3813)

---
+++ cli/pkg/upgrade/1_12_7.go
@@ -0,0 +1,50 @@
+package upgrade
+
+import (
+	"github.com/Masterminds/semver/v3"
+	"github.com/beclab/Olares/cli/pkg/certs"
+	"github.com/beclab/Olares/cli/pkg/common"
+	"github.com/beclab/Olares/cli/pkg/core/task"
+	"github.com/beclab/Olares/cli/version"
+)
+
+var version_1_12_7 = semver.MustParse("1.12.7")
+
+type upgrader_1_12_7 struct {
+	breakingUpgraderBase
+}
+
+func (u upgrader_1_12_7) Version() *semver.Version {
+	cliVersion, err := semver.NewVersion(version.VERSION)
+	if err != nil {
+		return version_1_12_7
+	}
+	if samePatchLevelVersion(version_1_12_7, cliVersion) && getReleaseLineOfVersion(cliVersion) == mainLine {
+		return cliVersion
+	}
+	return version_1_12_7
+}
+
+func (u upgrader_1_12_7) AddedBreakingChange() bool {
+	if u.Version().Equal(version_1_12_7) {
+		return true
+	}
+	return false
+}
+
+func (u upgrader_1_12_7) PrepareForUpgrade() []task.Interface {
+	tasks := make([]task.Interface, 0)
+	tasks = append(tasks, migrateContainerdConfigV3()...)
+	tasks = append(tasks, &task.LocalTask{
+		Name:    "CleanupK3sCertsRenewService",
+		Prepare: new(common.OnlyK3s),
+		Action:  new(certs.UninstallAutoRenewCerts),
+	})
+
+	tasks = append(tasks, u.upgraderBase.PrepareForUpgrade()...)
+	return tasks
+}
+
+func init() {
+	registerMainUpgrader(upgrader_1_12_7{})
+}
