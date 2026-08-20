37326ffb1879aeba85ad1ee0166c081c0ebd5838
oscdjj <brucedai999@gmail.com>
2026-08-20 08:57:56 +0000

    chore(l4): bump l4 image to v0.3.47

--- cli/pkg/upgrade/base.go
+++ cli/pkg/upgrade/base.go
@@ -161,7 +161,7 @@ func (u upgraderBase) UpgradeSystemComponents() []task.Interface {
 		},
 		&task.LocalTask{
 			Name:   "UpgradeL4BFLProxy",
-			Action: &upgradeL4BFLProxy{Tag: "v0.3.46"},
+			Action: &upgradeL4BFLProxy{Tag: "v0.3.47"},
 			Retry:  6,
 			Delay:  15 * time.Second,
 		},
