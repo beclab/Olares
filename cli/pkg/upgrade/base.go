c150e6e417c71253e69c78c5952d48ae2117d721
oscdjj <brucedai999@gmail.com>
2026-08-25 12:28:00 +0800

    chore(l4): bump beclab/l4-bfl-proxy image to v0.3.48
    Update Olares.yaml, BFL L4_PROXY_IMAGE_VERSION, and upgrade base task tag.

--- cli/pkg/upgrade/base.go
+++ cli/pkg/upgrade/base.go
@@ -159,7 +159,7 @@ func (u upgraderBase) UpgradeSystemComponents() []task.Interface {
 		},
 		&task.LocalTask{
 			Name:   "UpgradeL4BFLProxy",
-			Action: &upgradeL4BFLProxy{Tag: "v0.3.46"},
+			Action: &upgradeL4BFLProxy{Tag: "v0.3.48"},
 			Retry:  6,
 			Delay:  15 * time.Second,
 		},
