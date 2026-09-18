7239afc5ae0a3d235f157b537c1dc579061fbdd3
Meow33 <supermonkey03@163.com>
2026-09-18 21:50:25 +0800

    docs: revert LarePass documentation updates (#4168)
    * Revert "docs: update local access guides for LarePass host management (#4103)"
    
    This reverts commit 370d4cf78c2ca23df2ed8da3ca69bfc9d63fd5b6.
    
    * Revert "docs: add guide for fetching resources (#4071)"
    
    This reverts commit 290524601e26546eb633fa0d5acd797336c9403f.

--- docs/.vitepress/one.en.ts
+++ docs/.vitepress/one.en.ts
@@ -45,7 +45,7 @@ export const oneSidebar: DefaultTheme.Sidebar = {
           link: "/one/access-olares-via-vpn",
         },
         {
-          text: "Access Olares on your local network",
+          text: "Access Olares via .local domain",
           link: "/one/access-olares-via-local-domain",
         },
       ]
